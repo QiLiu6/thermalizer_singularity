771591898bc06732f8c5f99aa6faa2998b208fe4
CheeseCode <163666455+QiLiu6@users.noreply.github.com>
2025-03-28 16:46:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 description = "Thermalizer"
 version="0.0.1"
 
-setup(name="thermalizer_singularity",
+setup(name="thermalizer",
     version=version,
     description=description,
     url="https://github.com/QiLiu6/thermalizer_singularity.git",
